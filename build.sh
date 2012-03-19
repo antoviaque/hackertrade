1b8dc1138c74fc6913657c60861dd077e0127c29
Xavier Antoviaque <xavier@antoviaque.org>
2012-03-18 22:28:47 -0400

    Increased quality images

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 cd static
 for i in `ls body/` ; do cat header.html body/$i footer.html > $i ; done
 
-cd img
-./convert.sh
 
--- static/img/convert.sh
+++ static/img/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in `ls orig/homeslide/`; do convert -resize 500 -quality 85 -gravity center -extent 500x330 orig/homeslide/$i $i ; done
-for i in `ls orig/team/`; do convert -resize 147 -quality 85 -gravity center -extent 147x110 orig/team/$i $i ; done
+for i in `ls orig/homeslide/`; do convert -resize 500 -quality 90 -gravity center -extent 500x330 orig/homeslide/$i $i ; done
+for i in `ls orig/team/`; do convert -resize 147 -quality 90 -gravity center -extent 147x110 orig/team/$i $i ; done
 
